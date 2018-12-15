96b8f36d97eadecc74b39609d785729fbf0f4d6e
emersion <contact@emersion.fr>
2018-12-15 18:24:49 +0100

    Use better alpha value when filling framebuffer

--- main.c
+++ main.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
 				row[x * 4 + 0] = color[0];
 				row[x * 4 + 1] = color[1];
 				row[x * 4 + 2] = color[2];
-				row[x * 4 + 3] = 0xF0;
+				row[x * 4 + 3] = 0x80;
 			}
 		}
 	}
